2226546a20239e0878c7d18f394554731efc4561
Gopi Tambi <tambigopijs@gmail.com>
2025-07-31 12:19:53 +0530

    first deployement

--- Agentic_AI/Career_Agent.py
+++ Agentic_AI/Career_Agent.py
@@ -10,6 +10,7 @@ from langchain_core.tools import tool
 from tavily import TavilyClient
 from langgraph.graph import MessageGraph, END
 
+
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 SEARCH_API_KEY = os.getenv("TAVILY_API_KEY")
@@ -24,24 +25,25 @@ model = ChatGoogleGenerativeAI(
 
 search = TavilySearchResults(tavily_api_key=SEARCH_API_KEY)
 tools_list = [search]
-agent = initialize_agent(tools = tools_list, llm = model, verbose=True, agent = "zero-shot-react-description")
+agent = initialize_agent(tools = tools_list, llm = model, verbose=False, agent = "zero-shot-react-description")
 # agent.invoke('Who is the current CM of Andhra Pradesh?')
 
 
 tavily = TavilyClient(api_key=SEARCH_API_KEY)
 
 #------Tools--------
+
 @tool
 def parse_resume(text: str) -> str:
-  """Extract skills, roles, years of experience from resume text"""
-  prompt = f"Extract structured info like skils, roles, years of experience from this resume:\n\n{text}"
+  """Extract relevant skills, roles, and professional experience from the provided text."""
+  prompt = f"Extract structured info like skils, roles, years of experience from this text:\n\n{text}"
   return model.invoke([HumanMessage(content=prompt)]).content
 
 @tool
 def search_jobs_tavily(query: str) -> str:
-  """Search for jobs using Tavily and returns summarized results."""
+  """Search for related jobs using Tavily and returns simplified results."""
   results = tavily.search(query)
-  print(results)
+  #print(results)
   return results["results"]
 
 #-----nodes-----
@@ -53,21 +55,21 @@ def profile_node(state: list[BaseMessage]) ->List[BaseMessage]:
 
 def job_search_node(state: List[BaseMessage]) -> List[BaseMessage]:
   profile = state[-2].content
-  query = model.invoke([HumanMessage(content=f"Base on this profile, write a job search query:\n {profile},  keep the query very short for search engines to search")]).content
+  query = model.invoke([HumanMessage(content=f"Base on this profile, write a job search query:\n {profile}, keep the query very short for search engines to search")]).content
   job = search_jobs_tavily.invoke(query)
   return state + [AIMessage(content=f"Found Job\n {job}")]
 
 def matcher_node(state: List[BaseMessage]) -> List[BaseMessage]:
   profile = state[-2].content
   jobs = state[-1].content
-  prompt = f"given this profile:\n{profile}\n\n Rank and recommend top3 jobs from:\n{jobs}"
+  prompt = f"given this profile:\n{profile}\n\n Rank and recommend only top3 jobs from:\n{jobs}"
   ranked = model.invoke([HumanMessage(content=prompt)])
   return state + [ranked]
 
 def cover_letter_node(state: List[BaseMessage]) -> List[BaseMessage]:
   profile = state[-3].content
   best_job = state[-1].content
-  prompt = f"Write a personalized short cover letter for this job:\n\n Job Info:\n {best_job} \n\nBased on Profile:\n{profile}"
+  prompt = f"Write a personalized short cover letter for this job:\n\n Job Info:\n {best_job} \n\n Based on Profile:\n{profile}"
   letter = model.invoke([HumanMessage(content=prompt)])
   return state + [letter]
 
@@ -95,7 +97,7 @@ input_msg = HumanMessage(content=question)
 results = app.invoke([input_msg])
 st.markdown("Answer:")
 for msg in results:
-  st.write(f"{type(msg).__name__}: {msg.content}\n")
+  st.write(msg.content)
 
 # print("\n--- Output ---\n")
 # for msg in results:
